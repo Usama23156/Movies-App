39dbc9e445fa91706c8eee6ecd1d03f49e9599cf
Usama23156 <usamaelkady25@gmail.com>
2025-05-12 02:44:47 +0300

    Updates

--- src/component/NotFound/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import error from "../../assets/img/error.svg";
-
-export default function NotFound() {
-  return (
-    <img src={error} className="mx-auto my-4" alt="" />
-  )
-}
